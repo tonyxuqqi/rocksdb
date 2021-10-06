9686f71d7430b0d5ef1e8c6340f4b45ef36a0e4c
tonyxuqqi <tonyxuqi@outlook.com>
2021-10-07 02:28:54 +0800

    fix perf_timer and TARGETS file

--- monitoring/perf_timer.cc
+++ monitoring/perf_timer.cc
@@ -5,8 +5,8 @@ void PerfTimer::Stop() {
     if (start_) {
       uint64_t duration = time_now() - start_;
       if (statistics_ != nullptr) {
-        if (duration > ULL(1000000000)) {
-            printf("PerfTimer::Stop duration:%d, ticker_type:%d \n", duration, ticker_type_);
+        if (duration > 1000000000ull) {
+            printf("PerfTimer::Stop duration:%lld, ticker_type:%d \n", (long long)duration, ticker_type_);
         } else {
             RecordTick(statistics_, ticker_type_, duration);
         }
