a10d27d32cce35c2e7b373167a8358e007d32a65
tabokie <xy.tao@outlook.com>
2022-12-08 20:33:44 +0800

    avoid lock when alloc
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- memtable/write_buffer_manager.cc
+++ memtable/write_buffer_manager.cc
@@ -108,10 +108,10 @@ void WriteBufferManager::UnregisterDB(DB* db) {
     auto match = s->db == db;
     if (match) {
       auto key = (uint64_t)(s->cfd);
-      if (active_mem_by_cfd_[key] > 0) {
-        ROCKS_LOG_WARN(logger_, "WriteBufferManager::Leak %s %lu",
-                       cfd_names_[key].c_str(), active_mem_by_cfd_[key]);
-      }
+      // if (active_mem_by_cfd_[key] > 0) {
+      //   ROCKS_LOG_WARN(logger_, "WriteBufferManager::Leak %s %lu",
+      //                  cfd_names_[key].c_str(), active_mem_by_cfd_[key]);
+      // }
       // std::cout << "leak " << active_mem_by_cfd_[key] << std::endl;
     }
     return match;
@@ -157,15 +157,15 @@ void WriteBufferManager::ReserveMem(size_t mem, uint64_t key) {
   }
   if (local_size > 0) {
     memory_active_.fetch_add(mem, std::memory_order_relaxed);
-    std::lock_guard<std::recursive_mutex> lock(sentinels_mu_);
-    active_mem_by_cfd_[key] += mem;
-    if (cfd_names_.count(key) == 0) {
-      auto cfd = static_cast<ColumnFamilyData*>((void*)key);
-      ROCKS_LOG_WARN(
-          logger_,
-          "WriteBufferManager::Leak2 reserve mem but not registered %s",
-          cfd->GetLongName().c_str());
-    }
+    // std::lock_guard<std::recursive_mutex> lock(sentinels_mu_);
+    // active_mem_by_cfd_[key] += mem;
+    // if (cfd_names_.count(key) == 0) {
+    //   auto cfd = static_cast<ColumnFamilyData*>((void*)key);
+    //   ROCKS_LOG_WARN(
+    //       logger_,
+    //       "WriteBufferManager::Leak2 reserve mem but not registered %s",
+    //       cfd->GetLongName().c_str());
+    // }
     // std::cout << "reserve " << active_mem_by_cfd_[key] << std::endl;
   }
 }
@@ -198,8 +198,8 @@ void WriteBufferManager::ReserveMemWithCache(size_t mem) {
 void WriteBufferManager::ScheduleFreeMem(size_t mem, uint64_t key) {
   if (flush_size() > 0) {
     memory_active_.fetch_sub(mem, std::memory_order_relaxed);
-    std::lock_guard<std::recursive_mutex> lock(sentinels_mu_);
-    active_mem_by_cfd_[key] -= mem;
+    // std::lock_guard<std::recursive_mutex> lock(sentinels_mu_);
+    // active_mem_by_cfd_[key] -= mem;
   }
 }
 
@@ -246,41 +246,41 @@ void WriteBufferManager::MaybeFlushLocked(DB* this_db) {
   if (!ShouldFlush()) {
     return;
   }
-  {
-    auto now = std::chrono::system_clock::now();
-    std::chrono::duration<double> duration = now - start_time_;
-    if (duration.count() > 600.0) {
-      start_time_ = now;
-      auto map2 = active_mem_by_cfd_;
-      uint64_t approx_sum = 0;
-      uint64_t map_sum = 0;
-      for (auto& s : sentinels_) {
-        if (!s->deleted) {
-          uint64_t current_memory_bytes = std::numeric_limits<uint64_t>::max();
-          uint64_t oldest_time = std::numeric_limits<uint64_t>::max();
-          s->db->GetApproximateActiveMemTableStats(s->cf, &current_memory_bytes,
-                                                  &oldest_time);
-          ROCKS_LOG_WARN(
-              logger_, "WriteBufferManager::Report map=%luKB approx=%luKB",
-              map2[(uint64_t)s->cfd] / 1024, current_memory_bytes / 1024);
-          map_sum += map2[(uint64_t)s->cfd];
-          map2.erase((uint64_t)s->cfd);
-          approx_sum += current_memory_bytes;
-        }
-      }
-      uint64_t leaked_map_sum = 0;
-      for (auto& p : map2) {
-        ROCKS_LOG_WARN(logger_,
-                      "WriteBufferManager::Report missing in list %s %luKB",
-                      cfd_names_[p.first].c_str(), p.second / 1024);
-        leaked_map_sum += p.second;
-      }
-      ROCKS_LOG_WARN(
-          logger_,
-          "WriteBufferManager::Report approx=%luKB map=%luKB map-leaked=%luKB",
-          approx_sum / 1024, map_sum / 1024, leaked_map_sum / 1024);
-    }
-  }
+  // {
+  //   auto now = std::chrono::system_clock::now();
+  //   std::chrono::duration<double> duration = now - start_time_;
+  //   if (duration.count() > 600.0) {
+  //     start_time_ = now;
+  //     auto map2 = active_mem_by_cfd_;
+  //     uint64_t approx_sum = 0;
+  //     uint64_t map_sum = 0;
+  //     for (auto& s : sentinels_) {
+  //       if (!s->deleted) {
+  //         uint64_t current_memory_bytes = std::numeric_limits<uint64_t>::max();
+  //         uint64_t oldest_time = std::numeric_limits<uint64_t>::max();
+  //         s->db->GetApproximateActiveMemTableStats(s->cf, &current_memory_bytes,
+  //                                                 &oldest_time);
+  //         ROCKS_LOG_WARN(
+  //             logger_, "WriteBufferManager::Report map=%luKB approx=%luKB",
+  //             map2[(uint64_t)s->cfd] / 1024, current_memory_bytes / 1024);
+  //         map_sum += map2[(uint64_t)s->cfd];
+  //         map2.erase((uint64_t)s->cfd);
+  //         approx_sum += current_memory_bytes;
+  //       }
+  //     }
+  //     uint64_t leaked_map_sum = 0;
+  //     for (auto& p : map2) {
+  //       ROCKS_LOG_WARN(logger_,
+  //                     "WriteBufferManager::Report missing in list %s %luKB",
+  //                     cfd_names_[p.first].c_str(), p.second / 1024);
+  //       leaked_map_sum += p.second;
+  //     }
+  //     ROCKS_LOG_WARN(
+  //         logger_,
+  //         "WriteBufferManager::Report approx=%luKB map=%luKB map-leaked=%luKB",
+  //         approx_sum / 1024, map_sum / 1024, leaked_map_sum / 1024);
+  //   }
+  // }
   uint64_t total_active_mem = 0;
   // We only flush at most one column family at a time.
   // This is enough to keep size under control except when flush_size is
