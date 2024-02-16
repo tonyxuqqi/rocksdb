b957e9a3fb0170c4ab92c922fec05dfdb9a5278e
Qi Xu <tonyxuqqi@outlook.com>
2024-02-16 10:10:51 -0800

    add more logs
    Signed-off-by: Qi Xu <tonyxuqqi@outlook.com>

--- db/db_impl/db_impl_write.cc
+++ db/db_impl/db_impl_write.cc
@@ -1550,6 +1550,24 @@ IOStatus DBImpl::WriteToWAL(const WriteThread::WriteGroup& write_group,
     stats->AddDBStats(InternalStats::kIntStatsWriteWithWal, write_with_wal);
     RecordTick(stats_, WRITE_WITH_WAL, write_with_wal);
   }
+
+  if (log_writer->get_log_number() != logs_.back().number) {
+    ROCKS_LOG_INFO(immutable_db_options_.info_log,
+                   "Not writing to latest WAL: [%" PRIu64 ", %" PRIu64 "]",
+                   log_writer->get_log_number(), logs_.back().number);
+  }
+  if (!need_log_sync) {
+    for (auto& log : logs_) {
+      if (log_writer->get_log_number() == log.number) {
+        if (log.IsSyncing()) {
+          ROCKS_LOG_INFO(immutable_db_options_.info_log,
+                         "WAL is being Syncing and writting: [%" PRIu64 "",
+                         log_writer->get_log_number());
+        }
+        break;
+      }
+    }
+  }
   return io_s;
 }
 
